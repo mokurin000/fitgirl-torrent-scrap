2eca4255339d7968ea16bb21608fa33676bfe44e
mokurin000 <1348292515a@gmail.com>
2025-04-27 01:03:06 +0800

    enhance: count dirs resursively, support read file too

--- count-file-size/src/main.rs
+++ count-file-size/src/main.rs
@@ -1,30 +1,35 @@
-use std::{fs, path::Path};
+use std::{
+    fs::{self, read_dir},
+    path::{Path, PathBuf},
+};
 
 use humansize::BINARY;
 use librqbit_buffers::ByteBuf;
-use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator};
+
+#[derive(argh::FromArgs)]
+#[argh(description = "count file size from torrent metainfo")]
+struct Args {
+    #[argh(positional)]
+    torrent_paths: Vec<PathBuf>,
+}
 
 fn main() -> anyhow::Result<()> {
-    let torrents = fs::read_dir("./output")?;
-
-    let mut torrents_vec = Vec::new();
-    for entry in torrents {
-        let entry = entry?;
-        if !Path::new(&entry.file_name())
-            .extension()
-            .is_some_and(|ext| ext.to_str() == Some("torrent"))
-        {
-            continue;
-        }
-
-        let data = fs::read(entry.path())?;
-        torrents_vec.push(data);
+    let Args { torrent_paths } = argh::from_env();
+    let mut result = vec![];
+    for torrent_path in torrent_paths {
+        visit_dirs_or_file(torrent_path, &mut result)?;
     }
+
+    let torrents_vec: Vec<_> = result
+        .into_par_iter()
+        .filter_map(|path| fs::read(path).ok())
+        .collect();
 
     let total_length = torrents_vec
         .par_iter()
         .filter_map(|bytes| {
-            librqbit_core::torrent_metainfo::torrent_from_bytes::<ByteBuf>(&bytes).ok()
+            librqbit_core::torrent_metainfo::torrent_from_bytes::<ByteBuf>(bytes).ok()
         })
         .filter_map(|meta| {
             meta.info
@@ -35,7 +40,34 @@ fn main() -> anyhow::Result<()> {
         .sum::<u64>();
 
     let humansize = humansize::format_size(total_length, BINARY);
-    println!("{humansize}");
+    println!("Total: {humansize}");
+
+    Ok(())
+}
+
+fn visit_dirs_or_file(path: impl AsRef<Path>, append_to: &mut Vec<PathBuf>) -> anyhow::Result<()> {
+    let path = path.as_ref();
+    if path.is_file() {
+        append_to.push(path.to_path_buf());
+        return Ok(());
+    }
+
+    let dir = read_dir(path)?;
+    for entry in dir.flatten() {
+        let path = entry.path();
+
+        if path.is_dir() {
+            visit_dirs_or_file(path, append_to)?;
+        } else {
+            if path
+                .extension()
+                .and_then(|ext| ext.to_str())
+                .is_some_and(|ext| ext == "torrent")
+            {
+                append_to.push(path);
+            }
+        }
+    }
 
     Ok(())
 }
